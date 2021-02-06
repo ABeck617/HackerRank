488350d53f447e3d20933aee9cda7ab0f93c20bf
Anthony Beckford <abeckford532@gmail.com>
2021-02-06 10:49:06 -0500

    Update LoopsOne.java

--- LoopsOne.java
+++ LoopsOne.java
@@ -1,35 +1,35 @@
-# Objective
-#I n this challenge, we're going to use loops to help us do some simple math.
+// Objective
+//I n this challenge, we're going to use loops to help us do some simple math.
 
-# Task
-# Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: N x i = result.
+// Task
+// Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: N x i = result.
 
-# Input Format
+// Input Format
 
-# A single integer, .
+// A single integer, .
 
-# Constraints
+// Constraints
 
-# Output Format
+// Output Format
 
-# Print  lines of output; each line  (where ) contains the  of  in the form:
-# N x i = result.
+// Print  lines of output; each line  (where ) contains the  of  in the form:
+// N x i = result.
 
-# Sample Input
+// Sample Input
 
-# 2
-# Sample Output
+// 2
+// Sample Output
 
-# 2 x 1 = 2
-# 2 x 2 = 4
-# 2 x 3 = 6
-# 2 x 4 = 8
-# 2 x 5 = 10
-# 2 x 6 = 12
-# 2 x 7 = 14
-# 2 x 8 = 16
-# 2 x 9 = 18
-# 2 x 10 = 20
+// 2 x 1 = 2
+// 2 x 2 = 4
+// 2 x 3 = 6
+//2 x 4 = 8
+// 2 x 5 = 10
+// 2 x 6 = 12
+// 2 x 7 = 14
+// 2 x 8 = 16
+// 2 x 9 = 18
+// 2 x 10 = 20
 
 import java.io.*;
 import java.math.*;
